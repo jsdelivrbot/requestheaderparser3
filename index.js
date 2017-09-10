ece9eccbfd22a2f98a7ae152857ed61ebc53d0d8
Shehan Atukorala <ShehanAtukorala@Shehans-MacBook-Pro.local>
2017-09-10 12:48:14 -0400

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-//var cors = require('cors');
+var cors = require('cors');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-//app.use(cors());
+app.use(cors());
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
