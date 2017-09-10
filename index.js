65efc0a505001535d944fdbc0a01490eb0852871
Shehan Atukorala <ShehanAtukorala@Shehans-MacBook-Pro.local>
2017-09-10 12:42:48 -0400

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
-
+//var cors = require('cors');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
+//app.use(cors());
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+var api = 'api/whoami'
+
 app.get('/', function(request, response) {
+
   response.render('pages/index');
 });
 
