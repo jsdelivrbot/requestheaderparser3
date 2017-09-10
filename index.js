7dd3f6923982189a9c5da6e0d499a4e8218629e7
Shehan Atukorala <ShehanAtukorala@Shehans-MacBook-Pro.local>
2017-09-10 12:54:48 -0400

    removed cors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-var cors = require('cors');
+//var cors = require('cors');
 //var bodyParser = require('body-parser');
 app.set('port', (process.env.PORT || 5000));
 
 //create an instance of expression
 app.use(express.static(__dirname + '/public'));
-app.use(cors());
+//app.use(cors());
 //app.use(bodyParser.json());
 // views is directory for all template files
 app.set('views', __dirname + '/views');
