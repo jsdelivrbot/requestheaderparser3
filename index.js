f65bc51d7e7ae65332382d239c6e69828cf3e960
Shehan Atukorala <ShehanAtukorala@Shehans-MacBook-Pro.local>
2017-09-11 13:50:25 -0400

    added user-agent

--- index.js
+++ index.js
@@ -2,25 +2,31 @@ var express = require('express');
 var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var useragent = require('express-useragent')
 app.set('port', (process.env.PORT || 5000));
 
 //create an instance of expression
 app.use(express.static(__dirname + '/public'));
 app.use(cors());
 app.use(bodyParser.json());
+app.use(useragent.express());
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-var api = 'api/whoami'
+var api = '/api/whoami'
 
 app.get('/', function(request, response) {
   console.log('300');
-  response.render('pages/index');
+  response.render('pages/index.ejs');
 });
 
 app.get(api, function(request, response){
-  console.log("100");
+  var language = request.acceptsLanguage();
+  var software = "OS: " + request.useragent.os + ", Browser: " + request.useragent.browser;
+  var ipaddress = request.ip;
+
+  response.json({'ipaddress': ipaddress, 'software': software})
 });
 
 app.listen(app.get('port'), function() {
